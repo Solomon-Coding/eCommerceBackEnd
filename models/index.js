f771644a16d81ff0fefc08a7a3761555bddba46f
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-03-06 19:14:58 -0600

    Working category-routes script. It can update, add, and delete a category

--- models/index.js
+++ models/index.js
@@ -7,28 +7,29 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category,
   {
-    foreignKey: 'category_id'
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
   }
 );
 
 // Categories have many Products
 Category.hasMany(Product,
   {
-    foreignKey: 'category_id'
+    foreignKey: 'category_id',
   }
 );
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,
   {
-    through: 'product_tag'
+    through: 'product_tag',
   }
 );
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,
   {
-    through: 'product_tag'
+    through: 'product_tag',
   }
 );
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -35,26 +35,40 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(
+      req.body
+    )
+ 
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+try {
+  const categoryData = await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value
   try {
-    const categoryData = await Category.findByPk(req.params.id, {
-      include: [{ model: Product }],
-    });
-
-    if (!categoryData) {
-      res.status(404).json({ message: 'Category found with that id!' });
-      return;
-    }
-
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);

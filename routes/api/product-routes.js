09a5eba522d99fa1450f4db52e4d6a09afc59105
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-03-06 19:43:41 -0600

    Completed the routes. All that is left is clean up and the README

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -106,15 +106,11 @@ router.put('/:id', (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag }],
-    });
-
-    if (!productData) {
-      res.status(404).json({ message: 'Product found with that id!' });
-      return;
-    }
-
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
     res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
